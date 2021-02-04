80e972f359cb92c468c873e4c9abb0e63551ef6a
code-bebop <woonj2@gmail.com>
2021-02-04 20:25:35 +0900

    Feat #17 - MatchListItem를 날짜별로 묶어 표시하는 기능 추가
    같은 날 플레이 된 MatchListItem들을 그룹으로 나누어 표시하는 기능을
    추가했다.

--- src/containers/MatchListContainer.tsx
+++ src/containers/MatchListContainer.tsx
@@ -1,8 +1,10 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import styled from "styled-components";
 import MatchListItem from "../components/MatchListItem";
+import ensure from "../lib/ensure";
 import { RootState } from "../modules";
+import { MatchInfoT } from "../types/types";
 
 const MatchListContainerBlock = styled.ul`
   button {
@@ -12,6 +14,7 @@ const MatchListContainerBlock = styled.ul`
 
 const MatchListContainer = (): ReactElement => {
   console.log("MatchListContainer 렌더링");
+  const [matchDateDiffObject, setMatchDateDiffObject] = useState({});
 
   const { summonerInfo, matchInfoList } = useSelector(
     (state: RootState) => ({
@@ -21,9 +24,49 @@ const MatchListContainer = (): ReactElement => {
     shallowEqual
   );
 
+  useEffect(() => {
+    const _matchDateDiffObject = {};
+    matchInfoList?.forEach((matchInfo, index) => {
+      const matchDateDiff = Math.ceil(
+        (new Date().getTime() -
+          new Date(matchInfo.info.game_datetime).getTime()) /
+          (1000 * 3600 * 24)
+      );
+
+      if (!_matchDateDiffObject[matchDateDiff]) {
+        _matchDateDiffObject[matchDateDiff] = new Array(matchInfo);
+      } else {
+        _matchDateDiffObject[matchDateDiff] = _matchDateDiffObject[
+          matchDateDiff
+        ].concat(matchInfo);
+      }
+    });
+
+    setMatchDateDiffObject(_matchDateDiffObject);
+  }, [matchInfoList]);
+
   return (
     <MatchListContainerBlock>
-      {matchInfoList?.map((matchInfo, key) => {
+      {Object.keys(matchDateDiffObject).map(key => {
+        return (
+          <div key={key}>
+            <p>{key}일 전</p>
+            {matchDateDiffObject[key].map((matchInfo, index) => {
+              if (summonerInfo?.puuid !== undefined) {
+                return (
+                  <li key={index}>
+                    <MatchListItem
+                      matchInfo={matchInfo}
+                      puuid={summonerInfo?.puuid}
+                    />
+                  </li>
+                );
+              }
+            })}
+          </div>
+        );
+      })}
+      {/* {matchInfoList?.map((matchInfo, key) => {
         if (summonerInfo?.puuid !== undefined) {
           return (
             <MatchListItem
@@ -33,7 +76,7 @@ const MatchListContainer = (): ReactElement => {
             />
           );
         }
-      })}
+      })} */}
     </MatchListContainerBlock>
   );
 };
