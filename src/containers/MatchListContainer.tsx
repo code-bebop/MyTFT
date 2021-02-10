343960e6e658be37b315c1ac98a2432861b0d8a5
code-bebop <woonj2@gmail.com>
2021-02-10 16:32:51 +0900

    Style #8 - MatchListContainer의 전체적인 스타일코드 적용
    MatchListContainer의 background-color, margin, padding
    MatchListProfile의 디테일한 스타일코드를 적용했다.

--- src/containers/MatchListContainer.tsx
+++ src/containers/MatchListContainer.tsx
@@ -2,13 +2,36 @@ import React, { ReactElement, useEffect, useState } from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import styled from "styled-components";
 import MatchListItem from "../components/MatchListItem";
-import ensure from "../lib/ensure";
 import { RootState } from "../modules";
-import { MatchInfoT } from "../types/types";
 
 const MatchListContainerBlock = styled.ul`
+  padding: 30px 24px 0;
+  margin-left: 16px;
+  background-color: #202b43;
   button {
     color: black;
+  }
+`;
+
+const MatchDateDiffItem = styled.li`
+  margin-bottom: 25px;
+`;
+
+const MatchListProfile = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  p {
+    margin: 0;
+    line-height: 1;
+    &:first-child {
+      font-size: 16px;
+      font-weight: bold;
+    }
+    &:last-child {
+      padding-left: 18px;
+      font-size: 14px;
+      color: #89a0b5;
+    }
   }
 `;
 
@@ -26,7 +49,7 @@ const MatchListContainer = (): ReactElement => {
 
   useEffect(() => {
     const _matchDateDiffObject = {};
-    matchInfoList?.forEach((matchInfo, index) => {
+    matchInfoList?.forEach(matchInfo => {
       const matchDateDiff = Math.ceil(
         (new Date().getTime() -
           new Date(matchInfo.info.game_datetime).getTime()) /
@@ -49,21 +72,26 @@ const MatchListContainer = (): ReactElement => {
     <MatchListContainerBlock>
       {Object.keys(matchDateDiffObject).map(key => {
         return (
-          <div key={key}>
-            <p>{key}일 전</p>
-            {matchDateDiffObject[key].map((matchInfo, index) => {
-              if (summonerInfo?.puuid !== undefined) {
-                return (
-                  <li key={index}>
-                    <MatchListItem
-                      matchInfo={matchInfo}
-                      puuid={summonerInfo?.puuid}
-                    />
-                  </li>
-                );
-              }
-            })}
-          </div>
+          <MatchDateDiffItem key={key}>
+            <MatchListProfile>
+              <p>{key}일 전</p>
+              <p>{matchDateDiffObject[key].length} 게임</p>
+            </MatchListProfile>
+            <ul>
+              {matchDateDiffObject[key].map((matchInfo, index) => {
+                if (summonerInfo?.puuid !== undefined) {
+                  return (
+                    <li key={index}>
+                      <MatchListItem
+                        matchInfo={matchInfo}
+                        puuid={summonerInfo?.puuid}
+                      />
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+          </MatchDateDiffItem>
         );
       })}
       {/* {matchInfoList?.map((matchInfo, key) => {
